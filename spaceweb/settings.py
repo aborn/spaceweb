bc5e0bbfb0f29cc7d3df4b1e9bb7db976b59af25
aborn <loveaborn@foxmail.com>
2013-11-23 13:44:11 +0800

    initial spaceweb code

---
+++ spaceweb/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for spaceweb project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'spaceweb'
+
+SPIDER_MODULES = ['spaceweb.spiders']
+NEWSPIDER_MODULE = 'spaceweb.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'spaceweb (+http://www.yourdomain.com)'
